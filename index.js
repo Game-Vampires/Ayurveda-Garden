c5a79d1a9e6286cafc1d0e4e893ae89b56b09219
Dragan Cicic <your3mail>
2021-08-31 14:13:36 -0400

    buttons added

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-function draw() {
+
     const canvas = document.getElementById('garden-game');
     const ctx = canvas.getContext('2d');
 
-    imgScale = 700/400;
 
-    img.onload = function(){
-        ctx.drawImage( img, 0, 0, 150*imgScale, 150);
+// Butoons
 
-    };
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "EASY";
 
-    img.src = "/Images/garden-background-vector-4.jpeg"; 
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
 
-    console.log(img)
+
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "MEDIUM";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "HARD";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// End of Buttons
+
+const img = new Image();
+img.src = "/Images/frame-pic.jpeg"

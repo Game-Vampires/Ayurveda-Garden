abd4bb5953954c29ef0f4db2f348dc18ca6c1d62
Michael Nazareth <mcnazareth@gmail.com>
2021-08-31 14:29:45 -0400

    hi

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function draw() {
-    const canvas = document.getElementById('garden-game');
-    const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("garden-game");
+const ctx = canvas.getContext("2d");
 
-    imgScale = 700/400;
+imgScale = 700 / 400;
 
-    img.onload = function(){
-        ctx.drawImage( img, 0, 0, 150*imgScale, 150);
+img.onload = function () {
+  ctx.drawImage(img, 0, 0, 150 * imgScale, 150);
+};
 
-    };
-
-    img.src = "/Images/garden-background-vector-4.jpeg"; 
-
-    console.log(img)
+img.src = "/Images/garden-background-vector-4.jpeg";
